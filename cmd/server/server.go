403eca19b1f697b4a22bce69fe735413b8d4b206
lastdarknes <tim.m@creek.art>
2024-09-24 22:03:27 +0500

    register middlewares on init

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/LastDarkNes/go-dns/internal/delivery/rest"
+	"github.com/LastDarkNes/go-dns/pkg/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	middlewares.RegisterMiddlewares(app)
 	rest.AddV1RoutesToApp(app)
 	app.Listen(":3000")
 }
